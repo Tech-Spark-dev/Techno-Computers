7982cc81c42cc1b39a2ce2e5e5d5bd666cdbd680
Manoj M <manoj904378@gmail.com>
2023-09-23 00:06:16 +0530

    Update ENV.js

--- frontend/src/configs/ENV.js
+++ frontend/src/configs/ENV.js
@@ -1,1 +1,1 @@
-export const REACT_SERVER_URL=window.location.origin?'http://localhost:3000':'https://backendstage.onrender.com';
+export const REACT_SERVER_URL=window.location.origin=='http://localhost:3000'?'http://localhost:5000':'https://backendstage.onrender.com';
