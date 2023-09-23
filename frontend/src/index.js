76517c8b0b667c410089e81732cdc860426072cd
Ivin1998 <a.ivinaustan@gmail.com>
2023-09-23 10:03:50 +0530

    verifying the reload issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import Context from './Context';
@@ -9,11 +9,11 @@ import Context from './Context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <HashRouter>
+    <BrowserRouter>
       <Context>
         <App />
       </Context>
-    </HashRouter>
+    </BrowserRouter>
   </div>
 );
 
