d5a45c0b25785b44f00bb3bf1f339f2e8385bc84
Ivin1998 <a.ivinaustan@gmail.com>
2023-10-01 00:59:45 +0530

    updated footer logos

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import "../styles.css";
+import { SiInstagram,SiLinkedin } from "react-icons/si"; // Import the icons
+
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>&copy; {new Date().getFullYear()} Techno Computers E-commerce application | Developed by Ivin Austan</p>
+      <p>&copy; {new Date().getFullYear()} Techno Computers E-commerce application | Developed by Ivin Austan  &nbsp;
+      <a href='https://www.instagram.com/ivin_austan_ms/' target='_blank' className='socialmedialogos'><SiInstagram/> </a><a href='https://www.linkedin.com/in/ivinaustan/' target='_blank' className='socialmedialogos'><SiLinkedin/></a> </p>
     </div>
   )
 }
