9a375124f861668f644c2beb6cd16ce9832461b8
Ivin1998 <a.ivinaustan@gmail.com>
2023-10-02 01:11:52 +0530

    updating aboutus page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,17 +1,39 @@
-import React from 'react'
-import Footer from './Footer';
+import React from "react";
+import Footer from "./Footer";
+import { Col, Container, Row } from "react-bootstrap";
+import "../styles.css";
 
 const About = () => {
   return (
     <div>
-Techno Computers was established in July 2012, offering comprehensive servicing for both desktops and laptops, regardless of their condition. Additionally, we provide a wide range of computer accessories at budget-friendly prices.
-
-Our services extend beyond computer repairs, including facilities such as Eb bill payments, photocopying, printing services, exam application assistance, and various other computer-related tasks. We are committed to ensuring that our pricing remains affordable and accessible to all our valued customers.
-
-You can find us situated in Thammathu Konam, Nagercoil-4, serving the local community.
-      <Footer/>
+      <Container>
+        <Row>
+          <Col md={5}>
+            <img
+              src="technoboard1.jpg"
+              alt="techno"
+              className="productsimage"
+            />
+          </Col>
+          <Col md={6}>
+            <p className="aboutcontent">
+              Techno Computers was established in July 2012, offering
+              comprehensive servicing for both desktops and laptops, regardless
+              of their condition. Additionally, we provide a wide range of
+              computer accessories at budget-friendly prices. Our services
+              extend beyond computer repairs, including facilities such as Eb
+              bill payments, photocopying, printing services, exam application
+              assistance, and various other computer-related tasks. We are
+              committed to ensuring that our pricing remains affordable and
+              accessible to all our valued customers. You can find us situated
+              in Thammathu Konam, Nagercoil-4, serving the local community.
+            </p>
+          </Col>
+        </Row>
+        <Footer />
+      </Container>
     </div>
-  )
-}
+  );
+};
 
 export default About;
