1e8a4d2bf005b9bbc9ee76d217d26af0f9f6d814
Ivin1998 <a.ivinaustan@gmail.com>
2023-10-02 18:07:51 +0530

    updated aboutus page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -26,12 +26,14 @@ const About = () => {
               assistance, and various other computer-related tasks. We are
               committed to ensuring that our pricing remains affordable and
               accessible to all our valued customers. You can find us situated
-              in Thammathu Konam, Nagercoil-4, serving the local community.
-            </p>
+              in Thammathu Konam, Nagercoil-4, serving the local community. </p>
+              <br></br>
+              <b> Phone:</b> +91-9488978792<br/>
+             <b> Email: </b>technosoftware13@gmail.com
           </Col>
         </Row>
-        <Footer />
       </Container>
+      <Footer />
     </div>
   );
 };
